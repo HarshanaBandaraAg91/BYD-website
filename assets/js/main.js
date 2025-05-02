c7a9d1693b10685237b635b72d9bf98cca77285e
HarshanaBandaraAg91 <139755295+HarshanaBandaraAg91@users.noreply.github.com>
2025-05-02 22:24:07 +0530

    make about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,10 +31,24 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click',linkAction))
 
 /*=============== SWIPER HOME ===============*/ 
-
+const swiperHome = new Swiper('.home__swiper',{
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: (index, className) => {
+            return '<span class="' + className + '">' + String(index + 1).padStart(2, '0') + "</span>";
+        },
+    },
+})
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+    const header = document.getElementById('header')
 
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')                   
+}
+window.addEventListener('scroll', bgHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
