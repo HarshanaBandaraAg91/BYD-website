5d066d168782e501c549f0bd81b8acafb59e91e2
HarshanaBandaraAg91 <139755295+HarshanaBandaraAg91@users.noreply.github.com>
2025-05-08 12:12:19 +0530

    add scroolreveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,3 +81,19 @@ const scrollActive = () =>{
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+ const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 300,
+
+ })
+
+ sr.reveal('.home__bg' , {scale: 1.1, opacity: 1})
+ sr.reveal('.home__swiper' , {origin: 'right', distance: '300px' , delay: 800})
+ sr.reveal('.home__data' , {origin: 'bottom', distance: '120px' , delay: 1600})
+ sr.reveal('.swiper-pagination-bullet', {origin:'top', delay: 1800, opacity:0})
+ sr.reveal('home__button', {origin:'top', delay: 2200})
+ sr.reveal('.about__data, .contact__content', {origin: 'left'})
+ sr.reveal('.about__video, .contact__img', {origin: 'right'})
+ sr.reveal('.models__card', {interval: 100})
